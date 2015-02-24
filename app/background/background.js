827e071516cb8016d521f963b4c6b59b73f0c47f
Eric Volpert <ericvolp12@gmail.com>
2015-02-24 18:50:32 +0000

    Some Progress

--- app/background/background.js
+++ app/background/background.js
@@ -1,6 +1,13 @@
 $(function(){
-  $("table").addClass("table");
+  $("table").addClass("table table-striped");
   $("table").attr("border", 1);
   $("body").css("width", "75%");
   $("body").css("margin", "0 auto");
+  $("center > table > tbody > tr > td > center > b").html("<h2>" + $("center > table > tbody > tr > td > center > b").html() + "</h2>");
+  $("center > input").addClass("btn btn-default");
+  $("table").removeAttr("border");
+  $("table").css("border", "none");
+  $(":text").addClass("form-control");
+  $(":text").css("width", "25%");
+  $(":text").next().html("<div class='input-group-addon'>" + $(":text").next().html()+"</div>");
 });
